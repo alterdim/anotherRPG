7793fbf1a73a34b92a67e5d1e07d66fb3206cd69
Alternis <Alternis@DESKTOP-7TSVOLF>
2019-12-01 17:50:47 +0100

    level up !

--- JustAnotherRPG/src/character/Player.java
+++ JustAnotherRPG/src/character/Player.java
@@ -10,12 +10,19 @@ public class Player extends Character {
 	Gear leggingsGear = null;
 	Gear bootsGear = null;
 	int skillPoints = 0;
+	int level = 1;
+	int experience = 0;
+	int nextLevelXP = 1;
 	
-	Player(String name, int hp, int mp, int atk, int def, int sag) {
-		super(name, hp, mp, atk, def, sag);
+	Player(String name, int hp, int mp, int atk, int def, int sag, int spd) {
+		super(name, hp, mp, atk, def, sag, spd);
 	}
 	
 	public void levelUp() {
+		while (experience >= nextLevelXP) {
+			nextLevelXP = (int)((4 * (level ^ 3)) / 5);
+			skillPoints += 3;
+		}
 		
 	}
 	
